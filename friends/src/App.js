393f8990147952616b8250ca807be64b95041a9a
Matthew Taylor <majikpig@gmail.com>
2020-05-10 19:07:06 -0400

    routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Login from "./components/Login";
+import FriendForm from "./components/FriendForm";
+import FriendList from "./components/FriendList";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friend List</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/friends" component={FriendList} />
+          <PrivateRoute exact path="/friends/:friendId" component={FriendForm} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendForm.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export default function FriendForm() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <div></div>
+  );
+};
